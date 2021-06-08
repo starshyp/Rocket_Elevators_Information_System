65940240ccb9b96b839896650193ffe5e383757a
rayan303 <rayan303@gmail.com>
2021-06-08 10:25:54 -0400

    Revert "Revert "recreated new website""
    This reverts commit 95e5e6e094a7e3ebb41c5b88cd6a7411d7bd75d2.

---
+++ app/assets/javascripts/quote.js
@@ -0,0 +1,286 @@
+const inputsElementIds = [ 'number-of-apartments',
+'number-of-floors',
+'number-of-basements',
+'number-of-companies',
+'number-of-parking-spots',
+'number-of-elevators',
+'number-of-corporations',
+'maximum-occupancy',
+'business-hours',
+'elevator-amount', //read only
+'column-amount' //read only
+];
+
+const buildingTypes = {
+	commercial: ["number-of-floors", "number-of-basements","number-of-companies", "number-of-parking-spots", "number-of-elevators", "elevator-amount"],
+	residential: ["number-of-apartments", "number-of-floors", "number-of-basements", "elevator-amount", "column-amount"],
+	corporate: ["number-of-floors", "number-of-basements","number-of-parking-spots", "number-of-corporations", "maximum-occupancy", "elevator-amount", "column-amount"],
+	hybrid: ["number-of-floors", "number-of-basements", "number-of-companies", "number-of-parking-spots", "maximum-occupancy", "business-hours", "elevator-amount", "column-amount"]
+};
+
+//this will be updated
+const inputValues = {}
+
+
+function hideElementIds(elementIds)
+{
+	for(i=0; i<elementIds.length; i++)
+	{
+		$("#"+elementIds[i]).hide()
+	}
+}
+
+function showElementIds(elementIds)
+{
+	for(i=0; i<elementIds.length; i++)
+	{
+		$("#"+elementIds[i]).show()
+	}
+}
+
+function lookForTagInElement(element, tag)
+{
+	let contents = element.children();
+	for(let i=0; i<contents.length; i++)
+	{
+		let element2 = contents[i];
+		if(element2.nodeName.toLowerCase() === tag)
+		{
+			return element2;
+		}
+	}
+}
+
+let selectedBuilding = "residential";
+
+function calculateAmountOfColumns()
+{
+	switch(selectedBuilding) 
+	{
+		case "commercial":
+			return 0;
+		case "residential":
+			let floors = Number(inputValues["number-of-floors"].value);
+			let columns = Math.round(floors / 20);
+			return columns;
+	}
+	if(selectedBuilding == "hybrid" || selectedBuilding == "corporate")
+	{
+		let floors = Number(inputValues["number-of-floors"].value);
+		let basements = Number(inputValues["number-of-basements"].value);
+
+		let columns = Math.round((floors + basements) / 20);
+		return columns;
+	}
+	return 0;
+}
+
+function calculateAmountOfLifts()
+{
+	switch(selectedBuilding) 
+	{
+		case "commercial":
+			// the number of elevator shafts to be deployed is specified and the estimated number of cages is equal to the number required.
+			return inputValues["number-of-elevators"].value;
+		case "residential":
+			let apartments = Number(inputValues["number-of-apartments"].value);
+			let floors = Number(inputValues["number-of-floors"].value);
+			let basements = Number(inputValues["number-of-basements"].value);
+
+			let avgdoorsperfloor = apartments / floors;
+			let shafts = avgdoorsperfloor / 6;
+			
+			let additional = (floors / 20);
+			while(additional > 1)
+			{
+				shafts *= 2;
+				additional -= 1;
+			}
+			
+			shafts = Math.ceil(shafts);
+			return shafts;
+	}
+	if(selectedBuilding == "hybrid" || selectedBuilding == "corporate")
+	{
+		let floors = Number(inputValues["number-of-floors"].value);
+		let basements = Number(inputValues["number-of-basements"].value);
+		let occupancy = Number(inputValues["maximum-occupancy"].value);
+
+		let occupants = occupancy * (floors + basements);
+		let elevators = occupants / 1000;
+		let columns = Math.round((floors + basements) / 20);
+		let elevatorsPerColumn = Math.round(elevators / columns);
+		let finalAmountElevators = Math.round(elevatorsPerColumn * columns);
+		if(finalAmountElevators.toString() === "NaN")
+			return 1;
+		return finalAmountElevators;
+	}
+	return 0;
+}
+
+// 1= standard
+// 2= premium
+// 3= excelium
+function getProductLine()
+{
+	if(document.getElementById("standard").checked)
+	{
+		return 1;
+	}
+	else if(document.getElementById("premium").checked)
+	{
+		return 2;
+	}
+	else if(document.getElementById("excelium").checked)
+	{
+		return 3;
+	}
+	return "none";
+}
+
+function calculateUnitPrice()
+{
+	let productline = getProductLine();
+	switch(productline)
+	{
+		case 1:
+			return 7565;
+		case 2:
+			return 12345;
+		case 3:
+			return 15400;
+	}
+	//how would the runtime even reach here??
+	return 0;
+}
+
+function getInstallationFeesMultiplier()
+{
+	let productline = getProductLine();
+	switch(productline)
+	{
+		case 1:
+			return 1.1;
+		case 2:
+			return 1.13;
+		case 3:
+			return 1.16;
+	}
+	return 1;
+}
+
+function calculateInstallationPrice()
+{
+	let totalPrice = calculateElevatorTotalPrice();
+	let installscale = getInstallationFeesMultiplier();
+	return totalPrice * Math.max(installscale - 1, 0);
+}
+
+function calculateElevatorTotalPrice()
+{
+	let amountOfLifts = calculateAmountOfLifts();
+	let unitPrice = calculateUnitPrice();
+	
+	return (unitPrice * amountOfLifts);
+}
+
+function calculateTotalPrice()
+{
+	let elevatorTotalPrice = calculateElevatorTotalPrice();
+	
+	return elevatorTotalPrice * getInstallationFeesMultiplier();
+}
+
+let mf = new Intl.NumberFormat('en-CA', {style: 'currency',currency: 'CAD'});
+
+function setInputValue(id, v)
+{
+	lookForTagInElement($(id), "input").value = v;
+}
+
+function setPriceValue(id, number)
+{
+	let validation = number;
+	if(validation === Infinity)
+	{
+		validation = 0;
+	}
+	validation = Math.max(0, validation);
+
+	setInputValue(id, mf.format(validation).toString());
+}
+
+function UpdatePricing()
+{
+	setInputValue("#elevator-amount", calculateAmountOfLifts());
+	setInputValue("#column-amount", calculateAmountOfColumns());
+	setPriceValue("#elevator-unit-price", calculateUnitPrice());
+	setPriceValue("#elevator-total-price", calculateElevatorTotalPrice());
+	setPriceValue("#installation-fees", calculateInstallationPrice());
+	setPriceValue("#final-price", calculateTotalPrice());
+}
+
+function OnChanged()
+{
+	UpdatePricing();
+}
+
+const elementsToBind = [ 
+	'#building-type',
+	'#residential',
+	'#commercial',
+	'#corporate',
+	'#hybrid',
+	'#number-of-apartments',
+	'#number-of-floors',
+	'#number-of-basements',
+	'#number-of-companies',
+	'#number-of-parking-spots',
+	'#number-of-elevators',
+	'#number-of-corporations',
+	'#maximum-occupancy',
+	'#business-hours',
+	"#standard",
+	"#premium",
+	"#excelium",
+];
+
+function bindElements()
+{
+	for(let i=0; i<elementsToBind.length; i++)
+	{
+		$(elementsToBind[i]).on("change", 
+			function()
+			{
+				OnChanged();
+			}
+		);
+	}
+}
+
+//init js
+function OnReady()
+{
+	$("#building-type").on("change", function()
+		{
+			selectedBuilding = this.value;
+			hideElementIds(inputsElementIds);
+			showElementIds(buildingTypes[this.value]);
+		}
+	)
+	
+	selectedBuilding = $("#building-type").find("option:selected").attr("id");
+
+	hideElementIds(inputsElementIds);
+	showElementIds(buildingTypes[selectedBuilding]);
+	
+	for(let i=0; i<inputsElementIds.length; i++)
+	{
+		let id = inputsElementIds[i];
+		inputValues[id] = $("#"+id).find("input")[0];
+	}
+	
+	bindElements();
+}
+
+$(document).ready(OnReady)
---
+++ app/helpers/rocket_helper.rb
@@ -0,0 +1,2 @@
+module RocketHelper
+end
