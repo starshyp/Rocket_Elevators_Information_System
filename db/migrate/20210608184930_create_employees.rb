ff0c46e6487decec9b47ba39e016e786377a36d8
Amirbarzeg <abarzegarmo@gmail.com>
2021-06-08 14:52:11 -0400

    employees

---
+++ db/migrate/20210608184930_create_employees.rb
@@ -0,0 +1,9 @@
+class CreateEmployees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :employees do |t|
+      t.string :FirstName
+      t.string :LastName
+      t.string :Title
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_144616) do
+ActiveRecord::Schema.define(version: 2021_06_08_184930) do
+
+  create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "FirstName"
+    t.string "LastName"
+    t.string "Title"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
