6fcc384012b6fdccde07a270af05322097c077de
AlexisBTrepanier <alexisbtrepanier@hotmail.com>
2021-06-10 16:09:26 -0400

    deploy.rb cap version

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "AlexisBTrepanier"
 set :repo_url, "https://github.com/AlexisBTrepanier/Rocket_Elevators_Information_System"
+set :branch, "main"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
