6983793b303f66f472396cb9a4f6d24ae007ffe4
rayan303 <rayan303@gmail.com>
2021-06-10 09:38:58 -0400

    test post page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
   end
-  
+
+  def testpost
+    json_data = {:aaData=> [["abc","xyz","lmnop"],["sdf","adsdf","sadfsd"],["sdf","adsdf","sadfsd"]]}.to_json
+    respond_to do |format|
+      format.json { render :json => json_data }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
 	root "rocket#index"
   
-	get "/index.html", to: redirect("/index")
+	post "/home/search_exams", to: "home#search_exams"
+
 	get "/index", to: "rocket#index"
-	get "/quote.html", to: redirect("/quote")
 	get "/quote", to: "rocket#quote"
-	get "/residential.html", to: redirect("/residential")
 	get "/residential", to: "rocket#residential"
-	get "/commercial.html", to: redirect("/commercial")
 	get "/commercial", to: "rocket#commercial"
 
 	mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
