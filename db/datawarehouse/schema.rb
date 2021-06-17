a14621abedaf2b7186be78cf8cf8e24a6844584c
AlexisBTrepanier <alexisbtrepanier@hotmail.com>
2021-06-17 13:45:59 -0400

    rtdb before merge to main

--- db/datawarehouse/schema.rb
+++ db/datawarehouse/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_06_16_194213) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +46,11 @@ ActiveRecord::Schema.define(version: 2021_06_16_194213) do
     t.string "CompanyName"
     t.string "Email"
     t.integer "NbElevator"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
   end
 
 end
