d039e023240ee465796aaab1385d966e9af6707d
rayan303 <rayan303@gmail.com>
2021-06-30 14:29:15 -0400

    changed slack bot elevator status message

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,12 +1,12 @@
 class Elevator < ApplicationRecord
     belongs_to :column
 
-    after_update :after_updated_elevator
+    before_save :updated_elevator
 
-    def after_updated_elevator
-        if $SlackClient and self.saved_change_to_Status? then
-            
-            $SlackClient.chat_postMessage(channel: '#elevator_operations', text: "#{self.ElevatorType} Elevator #{self.Model} #{self.SerialNumber} Updated Info\nStatus: #{self.Status}", as_user: true)
+    def updated_elevator
+        if $SlackClient and self.Status_changed? then
+            text = "The Elevator #{self.id} with Serial Number #{self.SerialNumber} changed status from #{self.Status_change[0]} to #{self.Status_change[1]}"
+            $SlackClient.chat_postMessage(channel: '#elevator_operations', text: text, as_user: true)
         end
     end
 end
