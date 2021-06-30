447185549c321d2bad235766f6fada2b7d3606b3
rayan303 <rayan303@gmail.com>
2021-06-30 15:44:34 -0400

    slack bot removed old message event

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -5,19 +5,12 @@ class Elevator < ApplicationRecord
     belongs_to :column, optional: true
 
     before_save :updated_elevator
-    after_update :after_updated_elevator
     after_update :send_sms
 
     def updated_elevator
         if $SlackClient and self.Status_changed? then
             text = "The Elevator #{self.id} with Serial Number #{self.SerialNumber} changed status from #{self.Status_change[0]} to #{self.Status_change[1]}"
             $SlackClient.chat_postMessage(channel: '#elevator_operations', text: text, as_user: true)
-        end
-    end
-
-    def after_updated_elevator
-        if $SlackClient and self.saved_change_to_Status? then
-           $SlackClient.chat_postMessage(channel: "#elevator_operations", text: "#{self.ElevatorType} Elevator #{self.Model} #{self.SerialNumber} Updated Info\nStatus: #{self.Status}", as_user: true)
         end
     end
 
