3b6cc9d0283d76f8eb2d7b276a3432a96df65287
rayan303 <rayan303@gmail.com>
2021-07-02 12:44:00 -0400

    change env variables for twilio

--- send-sms.rb
+++ send-sms.rb
@@ -4,18 +4,21 @@ require 'sinatra'
 module Messenger
     class SMS
         def send_sms()
-            account_sid = ENV['TWILIO_ACCOUNT_SID']
-            auth_token = ENV['TWILIO_AUTH_TOKEN']
-            client = Twilio::REST::Client.new(account_sid, auth_token)
+            if ENV['TWILIO_ACCOUNT_SID'] && ENV['TWILIO_AUTH_TOKEN'] && ENV['TWILIO_ACCOUNT_PHONENUMBER'] && ENV['TWILIO_TARGET_PHONENUMBER'] then
+                
+                account_sid = ENV['TWILIO_ACCOUNT_SID']
+                auth_token = ENV['TWILIO_AUTH_TOKEN']
+                client = Twilio::REST::Client.new(account_sid, auth_token)
 
-            from = '+19412412411' # Your Twilio number
-            to = '+15149958192' # Your mobile phone number
+                from = ENV['TWILIO_ACCOUNT_PHONENUMBER'] # Your Twilio number
+                to = ENV['TWILIO_TARGET_PHONENUMBER']
 
-            client.messages.create(
-                from: from,
-                to: to,
-                body: "The status of elevator #{self.id} with serial number #{self.SerialNumber} has changed to Intervention!"
-            )
+                client.messages.create(
+                    from: from,
+                    to: to,
+                    body: "The status of elevator #{self.id} with serial number #{self.SerialNumber} has changed to Intervention!"
+                )
+            end
         end
     end
 end
