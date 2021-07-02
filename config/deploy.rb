6ffb17836076909e1873e12596bb55091a5b18bf
rayan303 <rayan303@gmail.com>
2021-07-02 16:19:45 -0400

    deploy use main branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "rayan303"
 set :repo_url, "https://github.com/rayan303/Rocket_Elevators_Information_System.git"
+set :branch, "main"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
