405008e3b55a10c34fd087e6bc3be280fecc616f
fexon145 <eminem_20_mai@hotmail.com>
2021-07-09 12:39:51 -0400

    Working good

--- db/migrate/20210615145213_create_batteries.rb
+++ db/migrate/20210615145213_create_batteries.rb
@@ -2,6 +2,7 @@ class CreateBatteries < ActiveRecord::Migration[5.2]
   def change
     create_table :batteries do |t|
       t.string :BType
+      t.string :Status
       t.date :DateOfCommissioning
       t.date :DateOfLastInspection
       t.string :CertificateOfOperations
